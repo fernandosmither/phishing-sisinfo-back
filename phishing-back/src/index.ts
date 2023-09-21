24a524e56fc5b36f6f8695d4d0f59f0d27df60ec
Fernando Smith <66154723+fernandosmither@users.noreply.github.com>
2023-09-21 01:52:06 -0300

    first working mvp

--- phishing-back/src/index.ts
+++ phishing-back/src/index.ts
@@ -1,44 +1,50 @@
-import { Hono } from 'hono'
-import type { MiddlewareHandler } from 'hono'
+import { Hono } from 'hono';
+import { cors } from 'hono/cors';
+import type { MiddlewareHandler } from 'hono';
 
 type AppContext = {
-  Bindings: {
-    sisinfo_phishing: KVNamespace
-  }
-}
+	Bindings: {
+		sisinfo_phishing: KVNamespace;
+	};
+};
 
-const app = new Hono<AppContext>().basePath('/')
+const app = new Hono<AppContext>().basePath('/');
+
+app.use('/*', cors({
+	origin: '*',
+	allowMethods: ['GET', 'POST', 'DELETE'],
+}));
 
 const countProtection: MiddlewareHandler<AppContext> = async (c, next) => {
-	const victims = await c.env.sisinfo_phishing.get("victims")
+	const victims = await c.env.sisinfo_phishing.get('victims');
 	if (victims === null) {
-		c.env.sisinfo_phishing.put("victims", "[]")
+		c.env.sisinfo_phishing.put('victims', '[]');
 	}
-  await next()
-}
+	await next();
+};
 
 app.get('/victims', countProtection, async (c) => {
-	const victims = await c.env.sisinfo_phishing.get("victims")
-  return c.json({
-    victims: victims
-  })
-})
+	const victims = await c.env.sisinfo_phishing.get('victims');
+	return c.json({
+		victims: victims,
+	});
+});
 
 app.post('/victim', countProtection, async (c) => {
-	const body = await c.req.parseBody()
-	const username = body.username
-	const victims = await c.env.sisinfo_phishing.get("victims")
-	await c.env.sisinfo_phishing.put("victims", JSON.stringify([...JSON.parse(victims!), username]))
+	const body = await c.req.parseBody();
+	const username = body.username;
+	const victims = await c.env.sisinfo_phishing.get('victims');
+	await c.env.sisinfo_phishing.put('victims', JSON.stringify([...JSON.parse(victims!), username]));
 	return c.json({
-		"moralOfTheStory": "Don't get phished!"
-	})
-})
+		moralOfTheStory: "Don't get phished!",
+	});
+});
 
 app.delete('/victims-secret-path', countProtection, async (c) => {
-	await c.env.sisinfo_phishing.put("victims", "[]")
+	await c.env.sisinfo_phishing.put('victims', '[]');
 	return c.json({
-		"moralOfTheStory": "Victims list reset!"
-	})
-})
+		moralOfTheStory: 'Victims list reset!',
+	});
+});
 
-export default app
+export default app;
